0cfe0058f9da389479a2e2ac110808a79d89d8b7
nikol <nikolskymisa@gmail.com>
2026-07-03 19:45:58 +0400

    Add local editor launcher

---
+++ open-editor.ps1
@@ -0,0 +1,4 @@
+﻿$root = Split-Path -Parent $MyInvocation.MyCommand.Path
+Start-Process powershell -ArgumentList '-NoExit','-Command',"cd `"$root`"; npm run dev" -WindowStyle Minimized
+Start-Sleep -Seconds 5
+Start-Process 'http://127.0.0.1:3000/editor'
